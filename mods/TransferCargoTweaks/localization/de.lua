c8e5906af6d8fe9941395c0c2b4ddf870f1372bc
Laserzwei <thembaandcoproductions@gmx.de>
2018-09-27 10:21:06 +0200

    German Translation

--- mods/TransferCargoTweaks/localization/de.lua
+++ mods/TransferCargoTweaks/localization/de.lua
@@ -1,4 +1,8 @@
 return {
+  ["%s Level %u"] = "%s Level %u", -- true translation would be "Stufe", but the game uses "Level" throughout
   ["Untrained %s"] = "Ungelernte %s",
-  ["Untrained %s /* plural */"] = "Ungelernte %s"
+  ["Untrained %s /* plural */"] = "Ungelernte %s",
+  ["You must be docked to the station to transfer crew."] = "Du musst an die Station gedockt sein, um die Mannschaft zu transferieren.",
+  ["You must be docked to the station to transfer cargo."] = "Du musst an die Station gedockt sein, um die Fracht zu übertragen.",
+  ["You must be docked to the station to transfer fighters."] = "Du musst an die Station gedockt sein, um die Jäger zu übertragen."
 }
